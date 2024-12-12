d2c3c2c3a88d5aa66e1c3445495ec638c47c23bd
Alejandro Clara <aleclara95@gmail.com>
2024-12-12 20:09:17 -0300

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Anonymous_Pro } from "next/font/google";
+import Script from "next/script"; // Import the Script component
 
 const anonymousPro = Anonymous_Pro({ weight: "400", subsets: ["latin"] });
 
@@ -16,6 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Add the Google Analytics script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1C9JKNR304"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1C9JKNR304');
+          `}
+        </Script>
+      </head>
       <body className={anonymousPro.className}>{children}</body>
     </html>
   );
